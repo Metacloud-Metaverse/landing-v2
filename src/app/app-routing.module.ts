8361803e47cf26a48e91c8b33cb73b1989755bb5
Axel Camiletti <45402442+axelcamiletti@users.noreply.github.com>
2022-01-17 16:00:10 -0300

    Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }, { path: 'news', loadChildren: () => import('./modules/news/news.module').then(m => m.NewsModule) }, { path: 'team', loadChildren: () => import('./modules/team/team.module').then(m => m.TeamModule) }, { path: 'partners', loadChildren: () => import('./modules/partners/partners.module').then(m => m.PartnersModule) }, { path: 'mc-core', loadChildren: () => import('./modules/mc-core/mc-core.module').then(m => m.McCoreModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/mc-core/mc-core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { McCoreRoutingModule } from './mc-core-routing.module';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    McCoreRoutingModule
+  ]
+})
+export class McCoreModule { }
