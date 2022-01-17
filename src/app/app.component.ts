4e23bf7bd5451bd353fd3b9dfcc882604aebf3ec
Axel Camiletti <45402442+axelcamiletti@users.noreply.github.com>
2022-01-17 15:53:00 -0300

    Angular + Material

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'metacloud-landing-v2';
+}
