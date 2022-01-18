f1cd1f5a48dea2f414cdc24fa189066ea3293bec
Axel Camiletti <45402442+axelcamiletti@users.noreply.github.com>
2022-01-18 20:32:33 -0300

    Blink effect

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,13 +1,25 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fade', [
+      state('inactive', style({ opacity: 0 })),
+      state('active', style({ opacity: 1 })),
+
+      transition('inactive <=> active', animate('500ms ease-in')),
+      transition('active <=> inactive', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   clickToStart = false;
+  activeBlink = false;
+  activeAnimation = 'inactive';
 
 
   constructor() { }
