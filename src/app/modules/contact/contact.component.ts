8888696745f499050e12779836e3bd17ff2fc1ba
Matias Camiletti <matias.camiletti@gmail.com>
2022-03-03 17:11:41 -0300

    Contact us function.

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -3,6 +3,7 @@ import { Meta, Title } from '@angular/platform-browser';
 import { FormGroup, FormControl } from '@angular/forms';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Timestamp } from "firebase/firestore";
 
 export interface WebContact {
   email: string;
@@ -27,13 +28,14 @@ export class ContactComponent implements OnInit {
     email: new FormControl(''),
     subject: new FormControl(''),
     message: new FormControl(''),
+    is_add_newsletter: new FormControl(false)
   });
 
   constructor(
     private titleService: Title,
     private metaTagService: Meta,
     private _snackBar: MatSnackBar,
-    //protected readonly firestore: AngularFirestore
+    protected readonly firestore: AngularFirestore
   ) { }
 
   ngOnInit(): void {
@@ -41,13 +43,22 @@ export class ContactComponent implements OnInit {
     this.metaTagService.updateTag(
       { name: 'description', content: 'If you have any questions or concerns, contact metacloud.' }
     );
+    this.loadDatabase();
   }
 
   loadDatabase() {
-    //this.itemsCollection = this.firestore.collection<WebContact>('web_contact');
+    this.itemsCollection = this.firestore.collection<WebContact>('web_contact');
   }
 
   onClickSubmit() {
+    let item = this.contactForm.value;
+    item.created_at = Timestamp.fromDate(new Date());
+    item.updated_at = Timestamp.fromDate(new Date());
+
+    console.log(item);
+
+    this.itemsCollection!.add(item);
+
     this.contactForm.reset();
 
     this._snackBar.open("Your message has been sent successfully", '', {
@@ -55,10 +66,6 @@ export class ContactComponent implements OnInit {
       horizontalPosition: 'right',
       verticalPosition: 'bottom',
       panelClass: 'snackbar_success'
-    });
-    //let item: WebContact = {
-
-    //}
-    //this.itemsCollection!.add(item);
+    }); 
   }
 }
