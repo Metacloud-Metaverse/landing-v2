6977b8ac9d8318ba8b69674ef0f1e6e50aef4f86
Axel Camiletti <45402442+axelcamiletti@users.noreply.github.com>
2022-03-25 12:17:01 -0300

    Our partners

--- src/app/modules/home/sections/step-k/step-k.component.ts
+++ src/app/modules/home/sections/step-k/step-k.component.ts
@@ -8,21 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class StepKComponent implements OnInit {
 
   logos:any = [
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
-    { title: '', img: '' },
+    { title: 'Hacken', img: 'logo-hacken.png' },
+    { title: 'Netvrk', img: 'logo-netvrk.png' },
+    { title: 'Dextools', img: 'logo-dextools.png' },
+    { title: 'Icodrops', img: 'logo-icodrops.png' },
+    { title: 'Coda', img: 'logo-coda.png' },
+    { title: 'Crowdcreate', img: 'logo-crowdcreate.png' },
+    { title: 'Binance Smart Chain', img: 'logo-binancesmartchain.png' },
+    { title: 'Wolfe', img: 'logo-wolfe.png' },
+    { title: 'Metawise', img: 'logo-metawise.png' },
+    { title: 'SafeGram', img: 'logo-safegram.png' },
+    { title: 'Velox', img: 'logo-velox.png' },
+    { title: 'Wall Street Hunters', img: 'logo-wallstreethunters.png' },
   ]
 
   constructor() { }
